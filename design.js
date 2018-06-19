828d78b68366b9e55073c708456118afd7885a5a
Sabrina Tolle <sabrinatolle@outlook.com>
2018-06-18 20:11:01 -0700

    Initial commit

---
+++ design.js
@@ -0,0 +1,60 @@
+var p1Button = document.getElementById("p1");
+var p2Button = document.getElementById("p2");
+var p1Score =0;
+var p1Display =document.querySelector("#p1Display");
+var p2Score = 0;
+var p2Display = document.querySelector("#p2Display");
+var gameOver = false;
+var winningScore = 5;
+var resetButton = document.getElementById("reset");
+var numInput = document.querySelector("input");
+var winningScoreDisplay = document.querySelector("p span");
+
+
+p1Button.addEventListener("click", function(){
+    if(!gameOver){
+        p1Score++;
+        if(p1Score === winningScore){
+            p1Display.classList.add("winner");
+            gameOver = true;
+        }
+    p1Display.textContent = p1Score;
+    }
+    
+})
+
+
+p2Button.addEventListener("click", function(){
+    if(!gameOver){
+        p2Score++;
+        if(p2Score === winningScore){
+            p2Display.classList.add("winner");
+            gameOver = true;
+        }
+    p2Display.textContent = p2Score;
+    }
+});
+
+resetButton.addEventListener("click", function(){
+   reset();
+});
+
+function reset(){
+    p1Score = 0;
+    p2Score = 0;
+    p1Display.textContent = 0;
+    p2Display.textContent = 0;
+    p1Display.classList.remove("winner");
+    p2Display.classList.remove("winner");
+    gameOver = false;
+
+}
+//change event will run anytime the value changes and it doesnt
+//matter how it changed.
+numInput.addEventListener("change", function(){
+    winningScoreDisplay.textContent = this.value;
+    winningScore = Number(this.value);
+    reset();
+
+
+});
